1de06d7cb0628e273f4129d0f5ae520e30d272ba
J. Daza <blackencio33@gmail.com>
2019-03-01 07:42:00 -0400

    cambios en file onedrive

--- src/utils/onedrive.js
+++ src/utils/onedrive.js
@@ -1,8 +1,11 @@
 const onedrive = require("onedrive-api");
 const fs = require("fs");
 
-const token = `eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFDRWZleFh4amFtUWIzT2VHUTRHdWd2WFIwb05KWEh6RDVKZkNHdHk1ZWhVTHFubnR0UTNSNk1qZGt5RGNDOXcwRFNDSExRTU1KQ2hIb1RBVm1pOElsOXY5NGFvQUlVTFhEMWUybktwcjU1eFNBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiLXN4TUpNTENJRFdNVFB2WnlKNnR4LUNEeHcwIiwia2lkIjoiLXN4TUpNTENJRFdNVFB2WnlKNnR4LUNEeHcwIn0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC82YmMyNzE5Yy1kZTVlLTRkOWUtYWVhMC00NzM1NTNmYzk1MmUvIiwiaWF0IjoxNTUxMDU0NTM5LCJuYmYiOjE1NTEwNTQ1MzksImV4cCI6MTU1MTA1ODQzOSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhLQUFBQW1oejVzdzZZdDdkWnRzUk5JVVVwaHd1VEZHVGdqTFBRTmJPNURKcW5JSnM9IiwiYW1yIjpbInB3ZCJdLCJhcHBfZGlzcGxheW5hbWUiOiJwZGYtdmlld2VyIiwiYXBwaWQiOiI3YTI2NDExZC04OTQ1LTQ5N2QtOWJkZi0xOTE4NThjNjRiZjAiLCJhcHBpZGFjciI6IjEiLCJmYW1pbHlfbmFtZSI6IlBhZHLDs24iLCJnaXZlbl9uYW1lIjoiSGFycnkiLCJpcGFkZHIiOiIxOTAuMjA2LjEwOC4yNCIsIm5hbWUiOiJIYXJyeSBQYWRyw7NuIiwib2lkIjoiOGIzYjNjYTEtYTUyMy00MTEyLTg5ZmQtY2Y4Njk3OTQ2MmE1Iiwib25wcmVtX3NpZCI6IlMtMS01LTIxLTE0NTQ0NzExNjUtNzkwNTI1NDc4LTcyNTM0NTU0My0xMjY1NzI5IiwicGxhdGYiOiIxNCIsInB1aWQiOiIxMDAzM0ZGRkFERDA0ODAwIiwic2NwIjoiQWdyZWVtZW50LlJlYWQuQWxsIEZpbGVzLlJlYWRXcml0ZS5BbGwgRmlsZXMuUmVhZFdyaXRlLkFwcEZvbGRlciBVc2VyLlJlYWQgVXNlci5SZWFkLkFsbCBVc2VyLlJlYWRCYXNpYy5BbGwgVXNlci5SZWFkV3JpdGUgVXNlci5SZWFkV3JpdGUuQWxsIHByb2ZpbGUgb3BlbmlkIGVtYWlsIiwic3ViIjoiQlVlcEF5N3lkcmxiV0RWOXc5aFlhdVZGWGlhVVRoY2daN21xVl9Sc3J4SSIsInRpZCI6IjZiYzI3MTljLWRlNWUtNGQ5ZS1hZWEwLTQ3MzU1M2ZjOTUyZSIsInVuaXF1ZV9uYW1lIjoiaFBhZHJvbkBuYXZpZXJhc2JyLmNvbSIsInVwbiI6ImhQYWRyb25AbmF2aWVyYXNici5jb20iLCJ1dGkiOiIxeUhJcTNNU1lVR29ISGRvVzdCX0FBIiwidmVyIjoiMS4wIiwieG1zX3N0Ijp7InN1YiI6IllIS3VNSWxZdy1CLUVwdjRvOU1lc19RWEo4NEFMaEFEbERBWVRhYzRjM0kifSwieG1zX3RjZHQiOjE1MzQxNzEzODZ9.Wp3fyt7XdJXIbBjMt6L5VtIGEXBKTBMp1bs4sHJSJe0506pdojZrEQ6OgFvBIyoNJobphXJnZEn3UgTT82fax0P-CGImkC1kTX1wO-Zbb1icXX4r_AeNT-0KkGTALjTXrmpkLOUcJ7OaO73zE5BKVdJe_PrjhnfZ3hexdPgpHsVJGur2g6UJQYXSGZtPAu-gPT9_4gkN8yqmQWtHQXtXgJsJEngBIvpoN0-xbiWs3WUHlgKvCBOkSuRW21MwJlJD02IJ-hd49kEnJbW3Rfn-Y1i0PXx_CsT3cbXxRsnFlQJN6HEv_vJimpi7Hg031WzzZYMZgzHrD69eCmN6OWZUyw`;
+const token = `eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFDRWZleFh4amFtUWIzT2VHUTRHdWd2RkNBWkV2TEMtZ2EzLXBEaG5BaGFGZTRMY1gtQ1F0RURCYlR5aG5VWjdXNGNDY2lvd1lIQkhFbEdmaGFTMkJBWThHTmpET1FwZF9LUnJFXzhtSll5WlNBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiLXN4TUpNTENJRFdNVFB2WnlKNnR4LUNEeHcwIiwia2lkIjoiLXN4TUpNTENJRFdNVFB2WnlKNnR4LUNEeHcwIn0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC82YmMyNzE5Yy1kZTVlLTRkOWUtYWVhMC00NzM1NTNmYzk1MmUvIiwiaWF0IjoxNTUxMjQ0MjI4LCJuYmYiOjE1NTEyNDQyMjgsImV4cCI6MTU1MTI0ODEyOCwiYWlvIjoiNDJKZ1lEaDdKNmxrM3NkblFna0xucXpZTWMvb0dRQT0iLCJhcHBfZGlzcGxheW5hbWUiOiJwZGYtdmlld2VyIiwiYXBwaWQiOiI3YTI2NDExZC04OTQ1LTQ5N2QtOWJkZi0xOTE4NThjNjRiZjAiLCJhcHBpZGFjciI6IjEiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC82YmMyNzE5Yy1kZTVlLTRkOWUtYWVhMC00NzM1NTNmYzk1MmUvIiwib2lkIjoiYzFkZTRjYTgtMGJmMC00NmQ2LWIyM2ItMWMwMDlkYzllMGI2Iiwicm9sZXMiOlsiVXNlci5SZWFkV3JpdGUuQWxsIiwiRmlsZXMuUmVhZFdyaXRlLkFsbCIsIlVzZXIuUmVhZC5BbGwiLCJGaWxlcy5SZWFkLkFsbCJdLCJzdWIiOiJjMWRlNGNhOC0wYmYwLTQ2ZDYtYjIzYi0xYzAwOWRjOWUwYjYiLCJ0aWQiOiI2YmMyNzE5Yy1kZTVlLTRkOWUtYWVhMC00NzM1NTNmYzk1MmUiLCJ1dGkiOiJyckNSbWhva3lVaWxpUWprdVZjWkFBIiwidmVyIjoiMS4wIiwieG1zX3RjZHQiOjE1MzQxNzEzODZ9.fn5c0q8FCPbCN7hLNWT10xz0GBJJb2nruSRLSxDmcffAt6vQatTPZ_jPNvwVmNUSX8i3YiEAYEdkLQm6DM-PCTs6vhQty1ovGkVIoBGbO3zMskOUyjIfIgzx7gaOKEduRVAPi_7abxMO37sREvX3PuuBN5qr_WTjOxa7lD98MPbWGEFgw0_mcw29bZZW4mjJ3IVangmW3ojCWOWZWyYqmQa4ZMBJZ8H3vKkw_Ub-15XoQ9G3tMDsxYDs5hfe9mcVUhoeTyJU0SK8n8HU3zaVv_IMHEAT11_3lUlQrSanKf7VA6MxseXL9hhrkZ06_xeKj06NjzpGWmqaZyDPG4TdBw`;
 const idFolderRoot = "017R6ELVIN6OMMV3ONY5CJXPIIKSYJRT4R";
+const idUser = '8b3b3ca1-a523-4112-89fd-cf86979462a5';
+const isShared = true 
+
 
 async function getFolders(id, name) {
   let response = null;
@@ -14,7 +17,9 @@ async function getFolders(id, name) {
   try {
     let childrens = await onedrive.items.listChildren({
       accessToken: token,
-      itemId: id
+      itemId: id,
+      shared: isShared,
+      user: idUser
     });
 
     //console.log(childrens)
@@ -41,7 +46,9 @@ async function getFiles(id) {
   try {
     let childrens = await onedrive.items.listChildren({
       accessToken: token,
-      itemId: id
+      itemId: id,
+      shared: isShared,
+      user: idUser
     });
 
     for (let item of childrens.value) {
@@ -76,7 +83,9 @@ exports.downloadFile = async function downloadFile(name, id) {
   await new Promise(async resolve => {
     let fileStream = await onedrive.items.download({
       accessToken: token,
-      itemId: id
+      itemId: id,
+      shared: isShared,
+      user: idUser
     });
 
     await fileStream
